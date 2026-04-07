ab61751a51e634f15aeeb94538a925c47b283864
AMATEUR-XX <chuyko_sa@mail.ru>
2026-04-07 20:41:09 +0700

    update 5

--- app/auth.py
+++ app/auth.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.models import User
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
